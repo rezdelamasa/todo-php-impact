c38852255e14321f09ab8d96f3317ed15892d813
rezdelamasa <rezdelamasa@gmail.com>
2024-02-08 16:48:27 -0500

    Database controller init

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+	include_once 'config.php';
+
+	class Database extends Config {
+		public function fetch() {
+
+		}
+	
+		public function insert() {
+
+		}
+
+		public function update() {
+
+		}
+		
+		public function delete() {
+
+		}
+	}
