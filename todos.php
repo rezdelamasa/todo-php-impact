b185c31b5e2193e4d957d369422649f79783b5e3
rezdelamasa <rezdelamasa@gmail.com>
2024-02-08 18:10:19 -0500

    setup delete route with id query param

--- todos.php
+++ todos.php
@@ -10,6 +10,8 @@
 
     $method = $_SERVER['REQUEST_METHOD'];
 
+    $id = intval($_GET['id'] ?? '');
+
     if ($method == 'GET') {
         $data = $todo->get();
         echo json_encode($data);
@@ -24,5 +26,14 @@
     }
 
     if ($method == 'DELETE') {
-
+        if ($id != null) {
+            if ($todo->delete($id)) {
+        
+                echo json_encode(["msg" => "Todo deleted successfully!"]);
+            } else {
+                echo json_encode(["msg" => "Todo deleteion failed!"]);
+            }
+        } else {
+            echo json_encode(["msg" => "Todo not found!"]);
+        }
     }
